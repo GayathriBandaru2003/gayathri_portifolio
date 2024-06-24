b46fd14c0f3ffa514fb07a3be438220ca4cca342
Anurag-verma18 <anuragkrverma18@gmail.com>
2024-06-24 12:04:29 +0530

    add skill logos to constants file

---
+++ src/utils/constants.js
@@ -0,0 +1,57 @@
+import { FaGithub } from "react-icons/fa";
+
+export const techStack = [
+    {
+        name: "ReactJs",
+        imgLogo: "https://res.cloudinary.com/dencbmqyy/image/upload/v1719146227/react-js-50_i5pqow.webp",
+    },
+    {
+        name: "JavaScript",
+        imgLogo: "https://res.cloudinary.com/dencbmqyy/image/upload/v1719146206/javascript-48_jlk7gr.webp",
+    },
+    {
+        name: "Redux",
+        imgLogo: "https://res.cloudinary.com/dencbmqyy/image/upload/v1719146239/redux-48_rfykrh.webp",
+    },
+    {
+        name: "TailwindCSS",
+        imgLogo: "https://res.cloudinary.com/dencbmqyy/image/upload/v1719146257/tailwindcss_plb3nl.webp",
+    },
+    {
+        name: "HTML5",
+        imgLogo: "https://res.cloudinary.com/dencbmqyy/image/upload/v1719146164/html-48_daxcel.webp",
+    },
+    {
+        name: "CSS3",
+        imgLogo: "https://res.cloudinary.com/dencbmqyy/image/upload/v1719146137/css3-48_azdrq5.webp",
+    },
+    {
+        name: "C++",
+        imgLogo: "https://res.cloudinary.com/dencbmqyy/image/upload/v1719146114/c_-48_eq2clu.webp",
+    },
+    {
+        name: "Vercel",
+        imgLogo: "https://res.cloudinary.com/dencbmqyy/image/upload/v1719146258/vercel_mngfzo.webp",
+    },
+    {
+        name: "Netlify",
+        imgLogo: "https://res.cloudinary.com/dencbmqyy/image/upload/v1719146216/netlify_unkqxg.webp",
+    },
+    {
+        name: "Linux",
+        imgLogo: "https://res.cloudinary.com/dencbmqyy/image/upload/v1719210747/linux-48_ptuzoe.webp",
+    },
+    {
+        name: "Github",
+        imgLogo: null,
+        elem: <FaGithub />
+    },
+    {
+        name: "Git",
+        imgLogo: "https://res.cloudinary.com/dencbmqyy/image/upload/v1719210746/git-48_zyvqnf.webp",
+    },
+    {
+        name: "Firebase",
+        imgLogo: "https://res.cloudinary.com/dencbmqyy/image/upload/v1719146148/firebase-48_mx4hro.webp",
+    }
+];
